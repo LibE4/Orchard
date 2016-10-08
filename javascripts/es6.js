3eef950bf3fcafec2a6839f766a341f7c5702de5
Bin Li <libin_75@hotmail.com>
2016-10-08 12:15:19 -0500

    grunt setup done

--- javascripts/es6.js
+++ javascripts/es6.js
@@ -1,3 +1,4 @@
+"use strict";
 console.log("es6");
 
 // const
@@ -15,7 +16,7 @@ console.log("b", b);
 function varTest(){
 	var x = 1;
 	if(true){
-		var x = 2;
+		x = 2;
 		console.log("inside x", x);
 	}
 	console.log("outside x", x);
@@ -36,22 +37,22 @@ letTest();
 var sum = function(){
 	let a = 1, b = 2;
 	return a + b;
-}
+};
 
 var sum2 =()=>{
 	let a = 1, b = 2;
 	return a + b;	
-}
+};
 
 var reflect = function(value){
 	return value;
-}
+};
 
 var reflect2 = (value)=>value;
 
 var realSum = function(num1, num2){
 	return num1 + num2;
-}
+};
 
 var realSum2 = (num1, num2)=>(num1 + num2);
 console.log("realSum2", realSum2(5, 4));
@@ -68,23 +69,23 @@ let myOldObject = {
 	wow: wow,
 	es6: es6,
 	numNum: numNum
-}
+};
 
 let myNewObject = {
 	wow, es6, numNum
-}
+};
 
 //method properties
 var e5Obj = {
 	foo: function(){console.log("foo");},
 	bar: function(){console.log("bar");}
-}
+};
 console.log("e5Obj", e5Obj);
 
 var e6Obj = {
 	foo(){console.log("foo");},
 	bar(){console.log("bar");}
-}
+};
 console.log("e6Obj", e6Obj);
 
 
---
+++ javascripts/main.js
@@ -0,0 +1,5 @@
+/*global $*/
+// use the line above if "$" is not in Gruntfile predef part
+"use strict";
+const outputDiv = $("#output");
+console.log("outputDiv",outputDiv);
